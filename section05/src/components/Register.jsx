f99af30f79d4518ff9a9073e44e193926e8bef49
smathj <smathj@naver.com>
2024-06-18 02:17:23 +0900

    useState 객체로 묶어서 사용하는방법

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -3,49 +3,59 @@
 // 2. 생년월일
 // 3. 국적
 // 4. 자기소개
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
-const Register = (prop) => {
+const Register = () => {
+  const [input, setInput] = useState({
+    name: '',
+    birth: '',
+    country: '',
+    bio: '',
+  })
 
+  // useRef 은 랜더링을 발생하지 않는다
+  const countRef = useRef(0)
+  const inputRef = useRef()
 
+  const { name, birth, country, bio } = input
 
-  const [name, setName] = useState('이름')
-  const [birth, setBirth] = useState('')
-  const [country, setCountry] = useState('')
-  const [bio, setBio] = useState('')
-
-  const onChangeName = (e) => {
-    setName(e.target.value)
+  const onChange = (e) => {
+    countRef.current++
+    console.log(countRef.current)
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    })
   }
 
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value)
-  }
-
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value)
-  }
-  const onChangeBio = (e) => {
-    setBio(e.target.value)
+  const onSubmit = () => {
+    // input.name 이 입력되지 않았을때 => 이름을 입력하는 DOM 요소 포커스
+    if (input.name === '') {
+      // inputRef.current 은 input 태그를 가리킴
+      inputRef.current.focus()
+      console.dir(inputRef.current)
+    }
   }
 
   return (
     <div>
       <div>
         <input
+          ref={inputRef}
+          name={'name'}
           placeholder={'이름'}
           type={'text'}
-          onChange={onChangeName}
+          onChange={onChange}
           value={name}
         />
       </div>
 
       <div>
-        <input type={'date'} onChange={onChangeBirth} value={birth} />
+        <input type={'date'} name='birth' onChange={onChange} value={birth} />
       </div>
 
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select value={country} onChange={onChange} name={'country'}>
           <option value=''></option>
           <option value='kr'>한국</option>
           <option value='us'>미국</option>
@@ -54,8 +64,10 @@ const Register = (prop) => {
       </div>
 
       <div>
-          <textarea value={bio} onChange={onChangeBio}/>
+        <textarea value={bio} onChange={onChange} name={'bio'} />
       </div>
+
+      <button onClick={onSubmit}>제출</button>
     </div>
   )
 }
