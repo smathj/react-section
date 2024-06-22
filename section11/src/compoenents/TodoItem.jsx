0b3178647f1d72fcbf722a7e2a8648242d7a0b65
smathj <smathj@naver.com>
2024-06-22 20:59:01 +0900

    React Router 를 이용한 CSR 페이지 이동

--- section11/src/compoenents/TodoItem.jsx
+++ section11/src/compoenents/TodoItem.jsx
@@ -26,18 +26,21 @@ const TodoItem = ({ id, isDone, content, date }) => {
 // export default TodoItem;
 
 // 고차 컴포넌트 (HOC)
-// export default memo(TodoItem, (prevProps, nextProps) => {
-//   // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
-//   // T -> Props 바뀌지 않음 -> 리렌더링 X
-//   // F -> Props 바뀜 -> 리렌더링 O
-//
-//   if (prevProps.id !== nextProps.id) return false
-//   if (prevProps.isDone !== nextProps.isDone) return false
-//   if (prevProps.content !== nextProps.content) return false
-//   if (prevProps.date !== nextProps.date) return false
-//
-//   return true
-// })
+/*
+export default memo(TodoItem, (prevProps, nextProps) => {
+  // 반환값에 따라, Props가 바뀌었는지 안바뀌었는지 판단
+  // T -> Props 바뀌지 않음 -> 리렌더링 X
+  // F -> Props 바뀜 -> 리렌더링 O
+
+  if (prevProps.id !== nextProps.id) return false
+  if (prevProps.isDone !== nextProps.isDone) return false
+  if (prevProps.content !== nextProps.content) return false
+  if (prevProps.date !== nextProps.date) return false
+
+  return true
+})
+ */
+
 
 // props 가 변하지않으면 캐싱으로 사용
 export default memo(TodoItem)
---
+++ section12/src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css'
+import { Link, Route, Routes, useNavigate } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import New from './pages/New.jsx'
+import Diary from './pages/Diary.jsx'
+import Notfound from './pages/Notfound.jsx'
+
+function App() {
+
+  // CSR 방식 이동
+  const nav = useNavigate()
+
+  const onClickButton = () => {
+    nav('/')
+  }
+
+  // 1. "/" : 모든 일기를 조회하는 Home 페이지
+  // 2. "/new" : 새로운 일기를 작성하는 New 페이지
+  // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+  return (
+    <>
+      {/*! 이 부분은 모든 페이지에 랜더링된다*/}
+      {/*! 공통 요소는 여기에 사용할 수 있겠지 */}
+      <div>
+        {/*a 태그 대체한다, CSR 방식 이동*/}
+        <Link to={'/'}>Home</Link>
+        <Link to={'/new'}>New</Link>
+        <Link to={'/diary'}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지 이동</button>
+      <Routes>
+        {/* Routes 컴포넌트 안에는 Route 컴포넌트만 사용할 수 있다 */}
+        <Route path={'/'} element={<Home />}></Route>
+        <Route path={'/new'} element={<New />}></Route>
+        <Route path={'/diary'} element={<Diary />}></Route>
+        <Route path={'*'} element={<Notfound />}></Route> {/*! switch 의 default 와 비슷함 */}
+      </Routes>
+    </>
+  )
+}
+
+export default App
